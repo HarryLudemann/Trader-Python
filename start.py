3eb96c3e686b99ece9abe0963e95391e8f093d85
HarryLudemann <60420781+HarryLudemann@users.noreply.github.com>
2021-08-05 12:55:54 +1200

    Update start.py

--- start.py
+++ start.py
@@ -13,25 +13,26 @@ START_TIME = time.time()
 TICKERS = Helper.Get_Tickers()                                # Load tickers from tickers.txt into list
 START_DATE, END_DATE = Helper.Get_Dates(Months=GET_MONTHS)    # Get end date(current) and start date(-Get Months) for daily data
 
-if not os.path.exists('Live-Data'):                           # Create folder to store live data if doesnt excists
+if not os.path.exists('Live-Data'):                           # Create folder to store live data if doesn't exists
     os.makedirs('Live-Data')
 
 Data.Create_Yfinance_Data(TICKERS, START_DATE, END_DATE)      # Create YFinance Data
 
-StockAlgorithms = Helper.Load_Algorithms()                     # list of Algorithm Objects from Algorithms dir
+StockAlgorithms = Helper.Load_Algorithms()                    # list of Algorithm Objects from Algorithms dir
 
 # while loop that runs once a minute
-DATA_DATE = END_DATE
+time.sleep(60)                                                # wait 60 seconds before first update
+DATA_DATE = END_DATE                                          # Set date to signal if daily data needs updating 
 while True:
     # Update Minute Data
-    Data.Update_Yfinance_Minute(TICKERS)
+    Data.Update_Yfinance_Minute(TICKERS)                      # Update minute data for yfinance
     print("Minute Data Updated")
 
 
     # Check if next day, update daily data
-    date = datetime.datetime.now().strftime("%Y-%m-%d")
-    if date != DATA_DATE:               
-        Data.Update_Yfinance_Daily(TICKERS)
+    date = datetime.datetime.now().strftime("%Y-%m-%d")       # get current date
+    if date != DATA_DATE:                                     # if date is different from data date update daily data
+        Data.Update_Yfinance_Daily(TICKERS)                   # Update daily data for yfinance
         print("Daily Data Updated")
 
 
